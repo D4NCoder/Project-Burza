01a36db2be462a0d58a344a5cd77d68d17d0c237
D4NCoder <vyhlidaldan@gmail.com>
2018-12-01 03:29:00 -0600

    Initial commit

---
+++ Controllers/TripController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Burza.Controllers
+{
+    public class TripController : Controller
+    {
+        public IActionResult Trips()
+        {
+            return View();
+        }
+    }
+}
